393b043f1a994cb0ec574845d54843b30b642ab4
Alexey Butkeev <abutkeev@netup.ru>
2023-08-27 21:56:46 +0300

    (front) collapse chapters

--- frontend/src/components/Player/chapters/index.tsx
+++ frontend/src/components/Player/chapters/index.tsx
@@ -1,6 +1,7 @@
 import { useContext } from 'react';
 import { PlayerStateContext, chapterChange } from '../usePlayerState';
 import Chapter from './Chapter';
+import { Accordion, AccordionDetails, AccordionSummary, Typography } from '@mui/material';
 
 const Chapters: React.FC = () => {
   const {
@@ -10,9 +11,18 @@ const Chapters: React.FC = () => {
   } = useContext(PlayerStateContext);
   const handleChapterClick = (chapter: number) => () => dispatch(chapterChange(chapter));
 
-  return chapters.map(({ title }, i) => (
-    <Chapter key={i} title={title} onClick={handleChapterClick(i)} current={currentChapter === i} />
-  ));
+  return (
+    <Accordion>
+      <AccordionSummary>
+        <Typography>Current chapter {chapters[currentChapter].title}</Typography>
+      </AccordionSummary>
+      <AccordionDetails>
+        {chapters.map(({ title }, i) => (
+          <Chapter key={i} title={title} onClick={handleChapterClick(i)} current={currentChapter === i} />
+        ))}
+      </AccordionDetails>
+    </Accordion>
+  );
 };
 
 export default Chapters;
