728a4fc4936c42b97256300d727f27e660bc537a
Alexey Butkeev <abutkeev@netup.ru>
2023-08-27 21:44:24 +0300

    :hammer: (front) move chapters to separate component

---
+++ frontend/src/components/Player/chapters/index.tsx
@@ -0,0 +1,18 @@
+import { useContext } from 'react';
+import { PlayerStateContext, chapterChange } from '../usePlayerState';
+import Chapter from './Chapter';
+
+const Chapters: React.FC = () => {
+  const {
+    state: { currentChapter },
+    chapters,
+    dispatch,
+  } = useContext(PlayerStateContext);
+  const handleChapterClick = (chapter: number) => () => dispatch(chapterChange(chapter));
+
+  return chapters.map(({ title }, i) => (
+    <Chapter key={i} title={title} onClick={handleChapterClick(i)} current={currentChapter === i} />
+  ));
+};
+
+export default Chapters;
--- frontend/src/components/Player/index.tsx
+++ frontend/src/components/Player/index.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
 import { Paper } from '@mui/material';
-import Chapter from './Chapter';
+import Chapters from './chapters';
 import { Book } from '../../api/api';
 import Controls from './controls';
-import usePlayerState, {
-  PlayerStateContext,
-  chapterChange,
-} from './usePlayerState';
+import usePlayerState, { PlayerStateContext } from './usePlayerState';
 
 interface PlayerProps {
   bookId: string;
@@ -15,16 +12,12 @@ interface PlayerProps {
 
 const Player: React.FC<PlayerProps> = ({ bookId, chapters }) => {
   const [{ state, audioRef }, dispatch] = usePlayerState(bookId, chapters);
-  const { currentChapter } = state;
-  const handleChapterClick = (chapter: number) => () => dispatch(chapterChange(chapter));
 
   return (
     <PlayerStateContext.Provider value={{ state, dispatch, chapters }}>
       <Paper sx={{ maxWidth: 'md', flexGrow: 1 }}>
         <Controls />
-        {chapters.map(({ title }, i) => (
-          <Chapter key={i} title={title} onClick={handleChapterClick(i)} current={currentChapter === i} />
-        ))}
+        <Chapters />
         <audio ref={audioRef} />
       </Paper>
     </PlayerStateContext.Provider>
