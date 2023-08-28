463521e0419c0f8a2394112b58efc0450fd87030
Alexey Butkeev <abutkeev@netup.ru>
2023-08-28 11:34:47 +0300

    (front) show home button if search params set

--- frontend/src/components/app-bar/AppBar.tsx
+++ frontend/src/components/app-bar/AppBar.tsx
@@ -1,16 +1,18 @@
 import { Home } from '@mui/icons-material';
 import { IconButton, Toolbar } from '@mui/material';
 import MuiAppBar from '@mui/material/AppBar';
-import { Outlet, useLocation, useNavigate } from 'react-router-dom';
+import { Outlet, useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 
 const AppBar: React.FC = () => {
   const navigate = useNavigate();
   const { pathname } = useLocation();
+  const [searchParams] = useSearchParams();
+
   return (
     <>
       <MuiAppBar position='fixed'>
         <Toolbar>
-          {pathname !== '/' && (
+          {(pathname !== '/' || searchParams.size !== 0) && (
             <IconButton color='inherit' onClick={() => navigate('/')}>
               <Home />
             </IconButton>
