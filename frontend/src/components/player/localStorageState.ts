d6021eb4fa910c7b62f8b7f1499deb2d22367716
Alexey Butkeev <abutkeev@netup.ru>
2023-08-28 18:50:04 +0300

    (front) add local storage books state

--- frontend/src/components/player/localStorageState.ts
+++ frontend/src/components/player/localStorageState.ts
@@ -2,42 +2,77 @@ import { useEffect } from 'react';
 import { PlayerState } from './usePlayerState';
 
 const playerStateName = 'playerState';
+const booksStateName = 'booksState';
+
+const parseSavedState = (name: string) => {
+  const state = localStorage.getItem(name);
+  if (!state) return {};
+  try {
+    const parsedState = JSON.parse(state);
+    if (!parsedState || typeof parsedState !== 'object') return {};
+    return parsedState;
+  } catch (e) {
+    console.error(`Can't parse ${name}`, e);
+    return {};
+  }
+};
+
+const updateBookState = (
+  bookId: string,
+  currentChapter: PlayerState['currentChapter'],
+  position: PlayerState['position']
+) => {
+  const state = parseSavedState(booksStateName);
+  state[bookId] = {
+    currentChapter,
+    position,
+    updated: new Date().toISOString(),
+  };
+  localStorage.setItem(booksStateName, JSON.stringify(state));
+};
+
+const loadSavedState = (bookId: string) => {
+  const state = parseSavedState(playerStateName);
+  if (!state.bookId || state.bookId !== bookId) {
+    const booksState = parseSavedState(booksStateName);
+    if (state.bookId && (state.currentChapter || state.position)) {
+      updateBookState(state.bookId, state.currentChapter, state.position);
+    }
+    if (booksState[bookId] && typeof booksState[bookId] === 'object') {
+      state.currentChapter = booksState[bookId].currentChapter;
+      state.position = booksState[bookId].position;
+    } else {
+      delete state.currentChapter;
+      delete state.position;
+    }
+  }
+  return state;
+};
 
 export const getSavedState = (initialState: PlayerState, bookId: string, chaptersLength: number) => {
   const state = { ...initialState };
-  const savedState = localStorage.getItem(playerStateName);
-  if (!savedState) return state;
-  try {
-    const {
-      currentChapter,
-      position,
-      bookId: savedBookId,
-      volume,
-      resetSleepTimerOnActivity,
-      preventScreenLock,
-    } = JSON.parse(savedState);
-    if (isFinite(volume) && volume > 0 && volume < 100) {
-      state.volume = volume;
-    }
+  const { currentChapter, position, volume, resetSleepTimerOnActivity, preventScreenLock } = loadSavedState(bookId);
 
-    if (typeof preventScreenLock === 'boolean') {
-      state.preventScreenLock = preventScreenLock;
-    }
-
-    if (typeof resetSleepTimerOnActivity === 'boolean') {
-      state.resetSleepTimerOnActivity = resetSleepTimerOnActivity;
-    }
-
-    if (savedBookId !== bookId) return state;
-    if (Number.isInteger(currentChapter) && currentChapter > 0 && currentChapter < chaptersLength) {
-      state.currentChapter = currentChapter;
-    }
-    if (isFinite(position) && position > 0) {
-      state.position = position;
-    }
-  } catch (e) {
-    console.error("Can't parse state", e);
+  if (isFinite(volume) && volume > 0 && volume < 100) {
+    state.volume = volume;
   }
+
+  if (typeof preventScreenLock === 'boolean') {
+    state.preventScreenLock = preventScreenLock;
+  }
+
+  if (typeof resetSleepTimerOnActivity === 'boolean') {
+    state.resetSleepTimerOnActivity = resetSleepTimerOnActivity;
+  }
+
+  if (Number.isInteger(currentChapter) && currentChapter > 0 && currentChapter < chaptersLength) {
+    state.currentChapter = currentChapter;
+  }
+
+  if (isFinite(position) && position > 0) {
+    state.position = position;
+  }
+
   return state;
 };
 
@@ -56,5 +91,6 @@ export const useSaveState = (state: PlayerState, bookId: string) => {
         updated: new Date().toISOString(),
       })
     );
+    updateBookState(bookId, currentChapter, position);
   }, [bookId, currentChapter, position, volume, resetSleepTimerOnActivity, preventScreenLock]);
 };
