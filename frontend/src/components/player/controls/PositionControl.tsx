cfb575023649526842bbb44f1cced21d581171a5
Alexey Butkeev <abutkeev@netup.ru>
2023-08-30 23:11:20 +0300

    :lipstick: (front) add skeleton to position control

--- frontend/src/components/player/controls/PositionControl.tsx
+++ frontend/src/components/player/controls/PositionControl.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import { PlayerStateContext, changePosition } from '../state/usePlayerState';
-import { Stack, Typography, Slider } from '@mui/material';
+import { Stack, Typography, Slider, Skeleton } from '@mui/material';
 import formatTime from '../../../utils/formatTime';
 
 const PositionControl: React.FC = () => {
@@ -11,19 +11,34 @@ const PositionControl: React.FC = () => {
   const handlePositionChange = (newPosition: number) => dispatch(changePosition(newPosition));
 
   return (
-    !!duration && (
-      <Stack spacing={2} direction='row' mx={1}>
-        <Typography>{formatTime(position)}</Typography>
-        <Slider
-          value={position}
-          onChange={(_, position) => typeof position === 'number' && handlePositionChange(position)}
-          max={duration}
-          valueLabelDisplay='auto'
-          valueLabelFormat={formatTime}
-        />
-        <Typography>{formatTime(duration)}</Typography>
-      </Stack>
-    )
+    <Stack spacing={2} alignItems='center' direction='row' mx={1}>
+      {!!duration ? (
+        <>
+          <Typography>{formatTime(position)}</Typography>
+          <Slider
+            value={position}
+            onChange={(_, position) => typeof position === 'number' && handlePositionChange(position)}
+            max={duration}
+            valueLabelDisplay='auto'
+            valueLabelFormat={formatTime}
+          />
+          <Typography>{formatTime(duration)}</Typography>
+        </>
+      ) : (
+        <>
+          <Skeleton variant='text'>
+            <Typography>00:00</Typography>
+          </Skeleton>
+          <Slider
+            disabled
+            componentsProps={{ thumb: { style: { display: 'none' } }, track: { style: { display: 'none' } } }}
+          />
+          <Skeleton>
+            <Typography>00:00</Typography>
+          </Skeleton>
+        </>
+      )}
+    </Stack>
   );
 };
 
