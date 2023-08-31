d65c6e6b516d114d31af1498ddf0c449044443a2
Alexey Butkeev <abutkeev@netup.ru>
2023-08-31 19:25:16 +0300

    (front) blur search text field on esc

--- frontend/src/components/app-bar/Search.tsx
+++ frontend/src/components/app-bar/Search.tsx
@@ -2,20 +2,25 @@ import { Clear, Search as SearchIcon } from '@mui/icons-material';
 import { InputAdornment, TextField } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../store';
 import { setSearchText } from '../../store/features/search';
-import { KeyboardEventHandler } from 'react';
+import { KeyboardEventHandler, useRef } from 'react';
 
 const Search: React.FC = () => {
   const { show, text } = useAppSelector(({ search }) => search);
   const dispatch = useAppDispatch();
+  const textfieldRef = useRef<HTMLInputElement>(null);
   const handleKeyDown: KeyboardEventHandler = ({ code }) => {
     if (code === 'Escape') {
       dispatch(setSearchText(''));
+      if (textfieldRef.current) {
+        textfieldRef.current.blur();
+      }
     }
   };
   return (
     show && (
       <TextField
         size='small'
+        inputProps={{ ref: textfieldRef }}
         InputProps={{
           sx: theme => ({
             backgroundColor: theme.palette.primary.dark,
