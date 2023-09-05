b4f7a17c20888ee856a8c25dd5ef7463e8693b1e
Alexey Butkeev <abutkeev@netup.ru>
2023-09-05 23:30:40 +0300

    :pencil: (front) add rangeRequests and cacheableResponse options for mp3

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -52,6 +52,8 @@ export default defineConfig({
             handler: 'CacheFirst',
             options: {
               cacheName: 'mp3',
+              cacheableResponse: { statuses: [200] },
+              rangeRequests: true,
             },
           },
         ],
