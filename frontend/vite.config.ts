be12fe83980271129c758dc031106043e957af10
Alexey Butkeev <abutkeev@netup.ru>
2023-09-17 17:11:25 +0300

    (front) proxy api to backend

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,62 +1,74 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: '',
-  plugins: [
-    react(),
-    VitePWA({
-      manifest: {
-        name: 'Audio books',
-        short_name: 'Audio books',
-        icons: [
-          {
-            src: 'pwa-64x64.png',
-            sizes: '64x64',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: 'pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any',
-          },
-          {
-            src: 'maskable-icon-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'maskable',
-          },
-        ],
+export default defineConfig(({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd(), '') };
+  const backendUrl = new URL(process.env.PROXY_TARGET || 'http://127.0.0.1:4000').toString();
+
+  return {
+    base: '',
+    server: {
+      proxy: {
+        '/api/': {
+          target: backendUrl,
+        },
       },
-      workbox: {
-        globPatterns: ['**/*.{js,css,html,woff2}'],
-        runtimeCaching: [
-          {
-            urlPattern: ({ url }) => url.pathname.startsWith('/api/') && !url.pathname.endsWith('.mp3'),
-            handler: 'NetworkFirst',
-            options: {
-              cacheName: 'api',
+    },
+    plugins: [
+      react(),
+      VitePWA({
+        manifest: {
+          name: 'Audio books',
+          short_name: 'Audio books',
+          icons: [
+            {
+              src: 'pwa-64x64.png',
+              sizes: '64x64',
+              type: 'image/png',
             },
-          },
-          {
-            urlPattern: ({ url }) => url.pathname.endsWith('.mp3'),
-            handler: 'CacheFirst',
-            options: {
-              cacheName: 'mp3',
-              cacheableResponse: { statuses: [200] },
-              rangeRequests: true,
+            {
+              src: 'pwa-192x192.png',
+              sizes: '192x192',
+              type: 'image/png',
             },
-          },
-        ],
-      },
-    }),
-  ],
+            {
+              src: 'pwa-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any',
+            },
+            {
+              src: 'maskable-icon-512x512.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'maskable',
+            },
+          ],
+        },
+        workbox: {
+          globPatterns: ['**/*.{js,css,html,woff2}'],
+          runtimeCaching: [
+            {
+              urlPattern: ({ url }) => url.pathname.startsWith('/api/') && !url.pathname.endsWith('.mp3'),
+              handler: 'NetworkFirst',
+              options: {
+                cacheName: 'api',
+              },
+            },
+            {
+              urlPattern: ({ url }) => url.pathname.endsWith('.mp3'),
+              handler: 'CacheFirst',
+              options: {
+                cacheName: 'mp3',
+                cacheableResponse: { statuses: [200] },
+                rangeRequests: true,
+              },
+            },
+          ],
+        },
+      }),
+    ],
+  };
 });
