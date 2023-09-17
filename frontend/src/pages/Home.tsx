0cbd923909bd69b9aa21a81d28f79f78efee4dcb
Alexey Butkeev <abutkeev@netup.ru>
2023-09-17 22:31:24 +0300

    :pencil: (front) fix error on back button pressed

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useGetBooksQuery } from '../api/api';
 import LoadingWrapper from '../components/common/LoadingWrapper';
@@ -8,13 +9,19 @@ export const currentBookVarName = 'currentBook';
 const Home: React.FC = () => {
   const currentBook = localStorage.getItem(currentBookVarName);
   const { data, isLoading, isError } = useGetBooksQuery();
+
   const navigate = useNavigate();
+  const initialRenderRef = useRef(true);
+
+  useEffect(() => {
+    if (initialRenderRef.current && currentBook && data && data.find(({ id }) => id === currentBook)) {
+      navigate(`/book/${currentBook}`, { replace: true });
+      initialRenderRef.current = false;
+    }
+  }, [currentBook, data]);
+
   if (isLoading || isError) {
     return <LoadingWrapper loading={isLoading} error={isError} />;
-  }
-  if (currentBook && data && data.find(({ id }) => id === currentBook)) {
-    navigate(`/book/${currentBook}`);
-    return;
   }
 
   return <BookList />;
