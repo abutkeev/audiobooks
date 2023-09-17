10f972f525ae2473b64626353314bc4cdc06d172
Alexey Butkeev <abutkeev@netup.ru>
2023-09-18 00:15:39 +0300

    (front) add mobile appbar

--- frontend/src/app/app-bar/AppBar.tsx
+++ frontend/src/app/app-bar/AppBar.tsx
@@ -1,10 +1,11 @@
 import { Home } from '@mui/icons-material';
-import { Box, IconButton, Toolbar, Typography } from '@mui/material';
+import { Box, IconButton, Toolbar, Typography, useMediaQuery, useTheme } from '@mui/material';
 import MuiAppBar from '@mui/material/AppBar';
 import { useLocation, useNavigate, useSearchParams } from 'react-router-dom';
 import Search from './Search';
 import { currentBookVarName } from '../../pages/Home';
 import { useAppSelector } from '../../store';
+import MobileAppBar from './MobileAppBar';
 
 const AppBar: React.FC = () => {
   const navigate = useNavigate();
@@ -18,11 +19,19 @@ const AppBar: React.FC = () => {
     navigate('/');
   };
 
+  const showHomeButton = !!token && (pathname !== '/' || new Set(searchParams.keys()).size !== 0);
+
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down('sm'));
+  if (mobile) {
+    return <MobileAppBar handleHomeButtonClick={handleHomeButtonClick} showHomeButton={showHomeButton} />;
+  }
+
   return (
     <>
       <MuiAppBar position='fixed'>
         <Toolbar>
-          {token && (pathname !== '/' || new Set(searchParams.keys()).size !== 0) && (
+          {showHomeButton && (
             <IconButton color='inherit' onClick={handleHomeButtonClick}>
               <Home />
             </IconButton>
--- frontend/src/app/app-bar/Search.tsx
+++ frontend/src/app/app-bar/Search.tsx
@@ -2,24 +2,49 @@ import { Clear, Search as SearchIcon } from '@mui/icons-material';
 import { InputAdornment, TextField } from '@mui/material';
 import { useAppDispatch, useAppSelector } from '../../store';
 import { setSearchText } from '../../store/features/search';
-import { KeyboardEventHandler, useRef } from 'react';
+import { KeyboardEventHandler, useEffect, useRef } from 'react';
 
-const Search: React.FC = () => {
+interface SearchProps {
+  hide?(): void;
+  fullWidth?: boolean;
+}
+
+const Search: React.FC<SearchProps> = ({ hide, fullWidth }) => {
   const { show, text } = useAppSelector(({ search }) => search);
   const dispatch = useAppDispatch();
   const textfieldRef = useRef<HTMLInputElement>(null);
+
+  useEffect(() => {
+    if (!textfieldRef.current || text || !hide) return;
+
+    textfieldRef.current.addEventListener('blur', hide);
+    return () => textfieldRef.current?.removeEventListener('blur', hide);
+  }, [text, hide]);
+
   const handleKeyDown: KeyboardEventHandler = ({ code }) => {
     if (code === 'Escape') {
       dispatch(setSearchText(''));
       if (textfieldRef.current) {
         textfieldRef.current.blur();
       }
+      if (hide) {
+        hide();
+      }
     }
   };
+
+  const handleClearText = () => {
+    dispatch(setSearchText(''));
+    if (hide) {
+      hide();
+    }
+  };
+
   return (
     show && (
       <TextField
         size='small'
+        fullWidth={fullWidth}
         inputProps={{ ref: textfieldRef }}
         InputProps={{
           sx: theme => ({
@@ -36,8 +61,8 @@ const Search: React.FC = () => {
               />
             </InputAdornment>
           ),
-          endAdornment: text && (
-            <InputAdornment position='end' onClick={() => dispatch(setSearchText(''))} sx={{ cursor: 'pointer' }}>
+          endAdornment: (text || hide) && (
+            <InputAdornment position='end' onClick={handleClearText} sx={{ cursor: 'pointer' }}>
               <Clear
                 sx={theme => ({
                   color: theme.palette.getContrastText(theme.palette.primary.dark),
