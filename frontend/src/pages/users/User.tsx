50800b8c97769ca32190e1650433f882003f16b4
Alexey Butkeev <abutkeev@netup.ru>
2023-09-25 23:05:45 +0300

    (front) user layout

--- frontend/src/pages/users/User.tsx
+++ frontend/src/pages/users/User.tsx
@@ -1,33 +1,49 @@
-import { Delete, ExpandMore } from '@mui/icons-material';
+import { Delete, ExpandMore, Shield } from '@mui/icons-material';
 import {
   Accordion,
   AccordionDetails,
   AccordionSummary,
   FormControl,
   FormControlLabel,
+  IconButton,
+  Stack,
   Switch,
   TextField,
   Tooltip,
   Typography,
 } from '@mui/material';
 import { UserDto } from '../../api/api';
+import CustomPassword from '../../components/common/CustomPassword';
 
 const User: React.FC<UserDto> = ({ id, login, name, enabled, admin }) => {
   return (
     <Accordion>
       <AccordionSummary expandIcon={<ExpandMore />}>
-        <Typography flexGrow={1} noWrap>
-          {login} {name && ` (${name})`}
-        </Typography>
-        <Tooltip title='Enabled'>
-          <Switch disabled checked={enabled} />
-        </Tooltip>
+        <Stack direction='row' flexGrow={1} alignItems='center'>
+          <Typography flexGrow={1} noWrap>
+            {login} {name && ` (${name})`}
+          </Typography>
+          {admin && (
+            <Tooltip title='Admin'>
+              <Shield />
+            </Tooltip>
+          )}
+          <Tooltip title={enabled ? 'Disable' : 'Enable'}>
+            <div>
+              <Switch disabled checked={enabled} />
+            </div>
+          </Tooltip>
+          <IconButton disabled>
+            <Delete />
+          </IconButton>
+        </Stack>
       </AccordionSummary>
       <AccordionDetails>
         <FormControl fullWidth>
           <Typography variant='body2'>ID: {id}</Typography>
-          <TextField label='Login' disabled required value={login} sx={{ mt: 2 }} />
           <TextField label='Name' disabled value={name} sx={{ mt: 2 }} />
+          <TextField label='Login' disabled required value={login} sx={{ mt: 2 }} />
+          <CustomPassword label='Password' disabled />
           <FormControlLabel control={<Switch checked={admin} disabled />} label='Admin' />
         </FormControl>
       </AccordionDetails>
