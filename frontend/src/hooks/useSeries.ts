ec2da30a9c66e21f705d8fcce361dc9575c1397d
Alexey Butkeev <abutkeev@netup.ru>
2023-09-28 22:44:24 +0300

    :fire: (front) use backend series api

--- frontend/src/hooks/useSeries.ts
+++ frontend/src/hooks/useSeries.ts
@@ -1,8 +1,8 @@
 import { useMemo } from 'react';
-import { useGetSeriesQuery } from '../api/api';
+import { useSeriesGetQuery } from '../api/api';
 
 const useSeries = () => {
-  const { data: seriesArray, isLoading: seriesLoading, isError: seriesError } = useGetSeriesQuery();
+  const { data: seriesArray, isLoading: seriesLoading, isError: seriesError } = useSeriesGetQuery();
   const series = useMemo(
     () => (seriesArray ? Object.fromEntries(seriesArray.map(({ id, name }) => [id, name])) : {}),
     [seriesArray]
