f8634821bd76881b25eb5987c88de52434a5b643
Alexey Butkeev <abutkeev@netup.ru>
2023-09-29 05:28:06 +0300

    (front) add required prop to CustomComboBox

--- frontend/src/components/common/CustomComboBox.tsx
+++ frontend/src/components/common/CustomComboBox.tsx
@@ -10,15 +10,16 @@ interface CustomComboBoxProps {
   value: string;
   setValue(v: string): void;
   sx?: SxProps;
+  required?: boolean;
 }
 
-const CustomComboBox: React.FC<CustomComboBoxProps> = ({ options, label, value, setValue, sx }) => {
+const CustomComboBox: React.FC<CustomComboBoxProps> = ({ options, label, value, setValue, sx, required = true }) => {
   const autoCompleteOptoons = useMemo(() => options.map(({ id, name: label }) => ({ id, label })), [options]);
   return (
     <Autocomplete
       sx={{ mt: 2, ...sx }}
       options={autoCompleteOptoons}
-      renderInput={params => <TextField {...params} label={label} required error={!value} />}
+      renderInput={params => <TextField {...params} label={label} required={required} error={required && !value} />}
       onChange={(_, v) => setValue(v ? v.id : '')}
       value={autoCompleteOptoons.filter(({ id }) => id === value)[0] || null}
     />
