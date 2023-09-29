c958aa8cce46c8efed9aeacf2adf4d76b0196d2a
Alexey Butkeev <abutkeev@netup.ru>
2023-09-29 05:57:37 +0300

    :pencil: (front) forward ref to link

--- frontend/src/components/common/Link.tsx
+++ frontend/src/components/common/Link.tsx
@@ -1,8 +1,9 @@
 import { Link as RouterLink } from 'react-router-dom';
 import { Link as MuiLink, LinkProps } from '@mui/material';
+import { forwardRef } from 'react';
 
-const Link: React.FC<Exclude<LinkProps<typeof RouterLink>, 'component'>> = ({ ...props }) => (
-  <MuiLink component={RouterLink} {...props} />
-);
+const Link: React.FC<Exclude<LinkProps<typeof RouterLink>, 'component'>> = forwardRef(({ ...props }, ref) => (
+  <MuiLink component={RouterLink} {...props} ref={ref} />
+));
 
 export default Link;
