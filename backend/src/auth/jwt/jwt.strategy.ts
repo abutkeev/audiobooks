5b685c259a7aaba4b0ea8e76bf2cc74028d51cea
Alexey Butkeev <abutkeev@netup.ru>
2023-10-03 17:06:43 +0300

    :hammer: (back) return user from db in jwt verify

--- backend/src/auth/jwt/jwt.strategy.ts
+++ backend/src/auth/jwt/jwt.strategy.ts
@@ -2,10 +2,11 @@ import { ExtractJwt, Strategy } from 'passport-jwt';
 import { PassportStrategy } from '@nestjs/passport';
 import { Injectable } from '@nestjs/common';
 import { UserDto } from 'src/users/dto/user.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(private usersService: UsersService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
@@ -14,12 +15,20 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
   }
 
   async validate(payload: any): Promise<UserDto> {
-    return {
-      id: payload.sub,
-      login: payload.username,
-      name: payload.name || '',
-      admin: !!payload.admin,
-      enabled: !!payload.enabled,
-    };
+    if (
+      process.env.INIT_ID &&
+      process.env.INIT_PASSWD &&
+      payload.sub === process.env.INIT_ID &&
+      payload.username === 'init'
+    ) {
+      return {
+        id: payload.sub,
+        login: payload.username,
+        name: payload.name || '',
+        admin: !!payload.admin,
+        enabled: !!payload.enabled,
+      };
+    }
+    return this.usersService.find(payload.sub);
   }
 }
