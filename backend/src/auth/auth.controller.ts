358a9477abae21c6a29bc4ff8b1251adf1b14716
Alexey Butkeev <abutkeev@netup.ru>
2023-10-04 17:27:20 +0300

    (back) add token generation method

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -1,15 +1,19 @@
-import { Controller, Post, Request, UseGuards } from '@nestjs/common';
+import { Controller, NotFoundException, Post, Request, UseGuards } from '@nestjs/common';
 import { LocalAuthGuard } from './local/local-auth.guard';
 import { AuthService } from './auth.service';
 import { Public } from './public.decorator';
 import { ApiBody, ApiCreatedResponse, ApiOperation, ApiTags, ApiUnauthorizedResponse } from '@nestjs/swagger';
 import { LoginBodyDto } from './dto/login-body.dto';
 import { LoginResponseDto } from './dto/login-response.dto';
+import { UsersService } from 'src/users/users.service';
 
 @Controller('auth')
 @ApiTags('auth')
 export class AuthController {
-  constructor(private authService: AuthService) {}
+  constructor(
+    private authService: AuthService,
+    private usersService: UsersService
+  ) {}
 
   @UseGuards(LocalAuthGuard)
   @Public()
@@ -22,5 +26,15 @@ export class AuthController {
   @ApiUnauthorizedResponse({ description: 'authorization failed' })
   async login(@Request() req) {
     return this.authService.login(req.user);
+  }
+
+  @Post('token')
+  @ApiOperation({ description: 'Generate new authorization token' })
+  async generateToken(@Request() { user: { id } }): Promise<LoginResponseDto> {
+    const user = await this.usersService.find(id);
+    if (!user) {
+      throw new NotFoundException(`user ${id} not found`);
+    }
+    return this.authService.login(user);
   }
 }
