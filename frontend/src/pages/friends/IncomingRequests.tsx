1a13470a4e16cd36f2429134c6e384b49a5037b5
Alexey Butkeev <abutkeev@netup.ru>
2023-11-07 01:04:32 +0300

    (front) add incoming request approve button

--- frontend/src/pages/friends/IncomingRequests.tsx
+++ frontend/src/pages/friends/IncomingRequests.tsx
@@ -1,11 +1,24 @@
 import { FC } from 'react';
-import { useFriendsGetIncomingRequestsQuery } from '../../api/api';
+import { useFriendsApproveRequestMutation, useFriendsGetIncomingRequestsQuery } from '../../api/api';
 import FriendsList from './FriendsList';
 
 const IncomingRequests: FC = () => {
-  const { data = [], isLoading, isError } = useFriendsGetIncomingRequestsQuery();
+  const { data = [], isLoading, isError, isFetching } = useFriendsGetIncomingRequestsQuery();
+  const [approve] = useFriendsApproveRequestMutation();
 
-  return <FriendsList data={data} isLoading={isLoading} isError={isError} emptyMessage='No requests' />;
+  const getApproveHandler = (id: string) => async () => {
+    await approve({ id });
+  };
+
+  return (
+    <FriendsList
+      data={data}
+      isLoading={isLoading}
+      isError={isError}
+      emptyMessage='No requests'
+      actions={[{ action: getApproveHandler, actionText: 'Approve', refreshing: isFetching }]}
+    />
+  );
 };
 
 export default IncomingRequests;
