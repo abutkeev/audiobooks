a0c00e2298440f4d73cd7927758b01cc95fc0bf0
Alexey Butkeev <abutkeev@netup.ru>
2023-11-06 09:15:05 +0300

    :pencil: (front) add title to not active page

--- frontend/src/pages/NotActive.tsx
+++ frontend/src/pages/NotActive.tsx
@@ -1,9 +1,13 @@
 import { Alert } from '@mui/material';
+import useTitle from '../hooks/useTitle';
 
-const NotActive: React.FC = () => (
-  <Alert severity='info' sx={{ maxWidth: 'md', mx: 'auto' }}>
-    Account is not active
-  </Alert>
-);
+const NotActive: React.FC = () => {
+  useTitle('Account is not active');
+  return (
+    <Alert severity='info' sx={{ maxWidth: 'md', mx: 'auto' }}>
+      Account is not active
+    </Alert>
+  );
+};
 
 export default NotActive;
