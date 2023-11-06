872c59693b3bae7f6cf974420799d3ac0918e6f6
Alexey Butkeev <abutkeev@netup.ru>
2023-11-06 20:05:01 +0300

    :lipsick: (front) formatting

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -3,7 +3,7 @@ import useTitle from '../hooks/useTitle';
 
 const NotFound: React.FC = () => {
   useTitle('Page not found');
-  
+
   return (
     <Alert severity='error' sx={{ maxWidth: 'md', mx: 'auto' }}>
       Page not found
