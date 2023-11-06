10a37f847ccd619b9347aca219ab3d2959f06571
Alexey Butkeev <abutkeev@netup.ru>
2023-11-06 09:13:52 +0300

    :pencil: (front) add title to 404 page

--- frontend/src/pages/NotFound.tsx
+++ frontend/src/pages/NotFound.tsx
@@ -1,9 +1,14 @@
 import { Alert } from '@mui/material';
+import useTitle from '../hooks/useTitle';
 
-const NotFound: React.FC = () => (
-  <Alert severity='error' sx={{ maxWidth: 'md', mx: 'auto' }}>
-    Page not found
-  </Alert>
-);
+const NotFound: React.FC = () => {
+  useTitle('Page not found');
+  
+  return (
+    <Alert severity='error' sx={{ maxWidth: 'md', mx: 'auto' }}>
+      Page not found
+    </Alert>
+  );
+};
 
 export default NotFound;
