284237dd2ce0bf8ee8633b9121911d8d865c778a
Alexey Butkeev <abutkeev@netup.ru>
2023-11-07 05:58:28 +0300

    :hammer: (back) move getFriendEntry to separate method

--- backend/src/friends/friends.service.ts
+++ backend/src/friends/friends.service.ts
@@ -1,6 +1,6 @@
 import { BadRequestException, ConflictException, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
-import { Model } from 'mongoose';
+import mongoose, { Model } from 'mongoose';
 import { UsersService } from 'src/users/users.service';
 import { FriendRequests } from './schemas/friend-requests.schema';
 import { FriendRequestDto } from './dto/friend-request.dto';
@@ -48,15 +48,17 @@ export class FriendsService {
     return true;
   }
 
+  private async getFriendEntry({ id, uid }: { id: mongoose.Types.ObjectId; uid: mongoose.Schema.Types.ObjectId }) {
+    const users = await this.usersService.findAll();
+    const { login, name } = users.find(({ id }) => id === uid.toString()) || {};
+    return { id: id.toString(), uid: uid.toString(), login, name };
+  }
+
   async getRequests(uid: string, type: 'in' | 'out'): Promise<FriendRequestDto[]> {
     const requests = await this.friendsRequestsModel.find(type === 'in' ? { to: uid } : { from: uid }).exec();
-    const users = await this.usersService.findAll();
-    return requests.map(({ _id, from, to }) => {
-      const id = _id.toString();
-      const uid = type === 'in' ? from.toString() : to.toString();
-      const { login, name } = users.find(({ id }) => id === uid) || {};
-      return { id, uid, login, name };
-    });
+    return Promise.all(
+      requests.map(({ _id, from, to }) => this.getFriendEntry({ id: _id, uid: type === 'in' ? from : to }))
+    );
   }
 
   async approve(uid: string, request_id: string): Promise<boolean> {
