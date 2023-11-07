bb4077c24b6e1968fcebaeb4b8613dd2bf85fbd9
Alexey Butkeev <abutkeev@netup.ru>
2023-11-07 05:16:32 +0300

    (front) hide outgoing friends requests empty tab

--- frontend/src/pages/friends/index.tsx
+++ frontend/src/pages/friends/index.tsx
@@ -4,7 +4,7 @@ import AddFriendToolbar from './AddFriendToolbar';
 import IncomingRequests from './IncomingRequests';
 import { Tab, Tabs } from '@mui/material';
 import OutgoingRequests from './OutgoingRequests';
-import { useFriendsGetIncomingRequestsQuery } from '../../api/api';
+import { useFriendsGetIncomingRequestsQuery, useFriendsGetOutgoingRequestsQuery } from '../../api/api';
 import LoadingWrapper from '../../components/common/LoadingWrapper';
 import FriendsBage from '../../components/FriendsBage';
 
@@ -12,21 +12,24 @@ const Friends: FC = () => {
   useTitle('Friends');
 
   const [tab, setTab] = useState<'in' | 'out'>('in');
-  const { data, isLoading, isError } = useFriendsGetIncomingRequestsQuery();
-  const friendsRequests = data?.length;
+  const { data: incoming, isLoading: incomingLoading, isError: incomingError } = useFriendsGetIncomingRequestsQuery();
+  const { data: outgoing, isLoading: outgoingLoading, isError: outgoingError } = useFriendsGetOutgoingRequestsQuery();
+
+  const loading = incomingLoading || outgoingLoading;
+  const error = incomingError || outgoingError;
 
   return (
     <>
       <AddFriendToolbar />
-      <LoadingWrapper loading={isLoading} error={isError}>
+      <LoadingWrapper loading={loading} error={error}>
         <Tabs value={tab} onChange={(_, newTab) => setTab(newTab)}>
           <Tab
             label='Incoming requests'
             value='in'
-            icon={<FriendsBage friendsRequests={friendsRequests} ml={1} />}
+            icon={<FriendsBage friendsRequests={incoming?.length} ml={1} />}
             iconPosition='end'
           />
-          <Tab label='Outgoing requests' value='out' />
+          {(tab === 'out' || outgoing?.length) && <Tab label='Outgoing requests' value='out' />}
         </Tabs>
         {tab === 'in' && <IncomingRequests />}
         {tab === 'out' && <OutgoingRequests />}
