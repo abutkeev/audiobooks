5e564a400d395a209397b1c07923ad90eb03b4ec
Alexey Butkeev <abutkeev@netup.ru>
2023-11-07 03:19:53 +0300

    :hammer: (front) use enum as tab index

--- frontend/src/pages/friends/index.tsx
+++ frontend/src/pages/friends/index.tsx
@@ -7,17 +7,18 @@ import OutgoingRequests from './OutgoingRequests';
 
 const Friends: FC = () => {
   useTitle('Friends');
-  const [tab, setTab] = useState(0);
+
+  const [tab, setTab] = useState<'in' | 'out'>('in');
 
   return (
     <>
       <AddFriendToolbar />
       <Tabs value={tab} onChange={(_, newTab) => setTab(newTab)}>
-        <Tab label='Incoming requests' value={0} />
-        <Tab label='Outgoing requests' value={1} />
+        <Tab label='Incoming requests' value='in' />
+        <Tab label='Outgoing requests' value='out' />
       </Tabs>
-      {tab === 0 && <IncomingRequests />}
-      {tab === 1 && <OutgoingRequests />}
+      {tab === 'in' && <IncomingRequests />}
+      {tab === 'out' && <OutgoingRequests />}
     </>
   );
 };
