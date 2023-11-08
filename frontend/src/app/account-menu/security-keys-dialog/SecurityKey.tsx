627fca53e3d6f9f890fcfedb3c45b0cfe1aaaeff
Alexey Butkeev <abutkeev@netup.ru>
2023-11-08 19:45:24 +0300

    (front) add webauthn key remove button

--- frontend/src/app/account-menu/security-keys-dialog/SecurityKey.tsx
+++ frontend/src/app/account-menu/security-keys-dialog/SecurityKey.tsx
@@ -1,4 +1,6 @@
 import { Paper, Stack, Typography } from '@mui/material';
+import DeleteButton from '../../../components/common/DeleteButton';
+import { useWebauthnRemoveMutation } from '../../../api/api';
 
 interface SecurityKeyProps {
   id: string;
@@ -6,12 +8,25 @@ interface SecurityKeyProps {
 }
 
 const SecurityKey: React.FC<SecurityKeyProps> = ({ name, id }) => {
+  const [remove] = useWebauthnRemoveMutation();
+
+  const handleRemove = async () => {
+    await remove({ id });
+  };
+
+  const keyName = name || id;
+
   return (
     <Paper square variant='outlined'>
-      <Stack key={id} spacing={1} direction='row' p={1}>
+      <Stack spacing={1} direction='row' px={1.5} alignItems='center'>
         <Typography noWrap flexGrow={1}>
-          {name || id}
+          {keyName}
         </Typography>
+        <DeleteButton
+          confirmationTitle='Remove security key?'
+          confirmationBody={`Remove security key ${keyName}`}
+          onConfirm={handleRemove}
+        />
       </Stack>
     </Paper>
   );
