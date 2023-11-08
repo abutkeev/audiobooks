ac08c951dc6805eef7288cd698800aade0b355cf
Alexey Butkeev <abutkeev@netup.ru>
2023-11-08 10:13:29 +0300

    (front) add chat authorization switch

--- frontend/src/pages/chats/ChatEntry.tsx
+++ frontend/src/pages/chats/ChatEntry.tsx
@@ -1,10 +1,22 @@
 import { FC } from 'react';
-import { ChatDto } from '../../api/api';
-import { Paper, Stack, Switch, Typography } from '@mui/material';
+import { ChatDto, useTelegramAuthorizeChatMutation, useTelegramUnauthorizeChatMutation } from '../../api/api';
+import { Paper, Stack, Typography } from '@mui/material';
 import ChatType from './ChatType';
 import ChatStatus from './ChatStatus';
+import CustomSwitch, { CustomSwitchProps } from '../../components/common/CustomSwitch';
 
-const ChatEntry: FC<ChatDto> = ({ type, title, status, authorized }) => {
+const ChatEntry: FC<ChatDto> = ({ id, type, title, status, authorized }) => {
+  const [authorize] = useTelegramAuthorizeChatMutation();
+  const [unauthorize] = useTelegramUnauthorizeChatMutation();
+
+  const handleChangeAuthorize: CustomSwitchProps['onChange'] = async newState => {
+    if (newState) {
+      await authorize({ id });
+      return;
+    }
+    await unauthorize({ id });
+  };
+
   return (
     <Paper square variant='outlined'>
       <Stack spacing={1} direction='row' p={1} alignItems='center'>
@@ -13,7 +25,11 @@ const ChatEntry: FC<ChatDto> = ({ type, title, status, authorized }) => {
         <Typography noWrap flexGrow={1}>
           {title}
         </Typography>
-        <Switch disabled checked={authorized} />
+        <CustomSwitch
+          tooltip={authorized ? 'Unauthorize' : 'Authorize'}
+          checked={authorized}
+          onChange={handleChangeAuthorize}
+        />
       </Stack>
     </Paper>
   );
