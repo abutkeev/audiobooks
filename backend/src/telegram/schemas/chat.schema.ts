419db483c16c1ef7ba4d605f05e5d187e663021e
Alexey Butkeev <abutkeev@netup.ru>
2023-11-08 05:54:42 +0300

    :pencil: (back) remove mongo id from telegram chat

--- backend/src/telegram/schemas/chat.schema.ts
+++ backend/src/telegram/schemas/chat.schema.ts
@@ -3,7 +3,6 @@ import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 @Schema({
   toJSON: {
     transform: (_, ret) => {
-      ret.id = ret._id.toString();
       delete ret._id;
       delete ret.__v;
     },
