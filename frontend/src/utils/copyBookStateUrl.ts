466e8575811e4bc0a9296f6e7b7f0e60a1f51cd1
Alexey Butkeev <abutkeev@netup.ru>
2023-11-13 16:58:03 +0300

    (front) copyBookStateUrl localization

--- frontend/src/utils/copyBookStateUrl.ts
+++ frontend/src/utils/copyBookStateUrl.ts
@@ -2,9 +2,10 @@ import copy from 'copy-to-clipboard';
 import { store } from '../store';
 import { BookState } from '../store/features/player';
 import { addSnackbar } from '../store/features/snackbars';
+import { t } from 'i18next';
 
 const copyBookStateUrl = ({ currentChapter, position }: BookState) => {
   copy(`${location.toString()}?currentChapter=${currentChapter}&position=${position}`);
-  store.dispatch(addSnackbar({ severity: 'success', text: 'Copied to clipboard', timeout: 2000 }));
+  store.dispatch(addSnackbar({ severity: 'success', text: t('Copied to clipboard'), timeout: 2000 }));
 };
 export default copyBookStateUrl;
