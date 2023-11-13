5c51f027f41555db110d35b4779695b82f5a27e7
Alexey Butkeev <abutkeev@netup.ru>
2023-11-13 13:11:25 +0300

    (front) inactive page localization

--- frontend/src/pages/NotActive.tsx
+++ frontend/src/pages/NotActive.tsx
@@ -4,9 +4,11 @@ import { useTgGetAccountInfoQuery, useTgSetAuthDataMutation } from '../api/api';
 import TelegramAuthButton, { TelegramAuthCallback } from '../components/TelegramAuthButton';
 import LoadingWrapper from '../components/common/LoadingWrapper';
 import { Telegram } from '@mui/icons-material';
+import { useTranslation } from 'react-i18next';
 
 const NotActive: React.FC = () => {
-  useTitle('Account is not active');
+  const { t } = useTranslation();
+  useTitle(t('Account is not active'));
 
   const { data: telegramAccount, isLoading, isError, isFetching } = useTgGetAccountInfoQuery();
   const [setTgAuth] = useTgSetAuthDataMutation();
@@ -18,30 +20,32 @@ const NotActive: React.FC = () => {
   return (
     <Container maxWidth='sm'>
       <Stack spacing={1}>
-        <Alert severity='info'>Account is not active. Contact site administrator to activate it.</Alert>
+        <Alert severity='info'>{t('Account is not active. Contact site administrator to activate it.')}</Alert>
         <LoadingWrapper loading={isLoading} error={isError}>
           {telegramAccount?.info ? (
             <Stack spacing={1}>
               <Alert severity='warning'>
-                Automatic activation failed. If you are authorized chat member, please retry.
+                {t('Automatic activation failed. If you are authorized chat member, please retry.')}
               </Alert>
               <TelegramAuthButton
                 onAuth={handleTelegramAuth}
                 refreshing={isFetching}
                 progressButtonProps={{ buttonProps: { fullWidth: true, startIcon: <Telegram /> } }}
               >
-                Retry
+                {t('Retry')}
               </TelegramAuthButton>
             </Stack>
           ) : (
             <Stack spacing={1}>
-              <Alert severity='info'>Authorized telegram chat members can activate their accounts automatically.</Alert>
+              <Alert severity='info'>
+                {t('Authorized telegram chat members can activate their accounts automatically.')}
+              </Alert>
               <TelegramAuthButton
                 onAuth={handleTelegramAuth}
                 refreshing={isFetching}
                 progressButtonProps={{ buttonProps: { fullWidth: true, startIcon: <Telegram /> } }}
               >
-                Link telegram account and try to activate
+                {t('Link telegram account and try to activate')}
               </TelegramAuthButton>
             </Stack>
           )}
