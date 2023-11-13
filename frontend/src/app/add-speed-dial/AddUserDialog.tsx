f699541ccf6cc0cfef784d3112285005102c3ad9
Alexey Butkeev <abutkeev@netup.ru>
2023-11-13 19:29:49 +0300

    (front) create user dialog localization

--- frontend/src/app/add-speed-dial/AddUserDialog.tsx
+++ frontend/src/app/add-speed-dial/AddUserDialog.tsx
@@ -6,6 +6,7 @@ import { addSnackbar } from '../../store/features/snackbars';
 import { useUsersCreateMutation, useUsersGetAllQuery } from '../../api/api';
 import LoadingWrapper from '../../components/common/LoadingWrapper';
 import CustomPassword from '../../components/common/CustomPassword';
+import { useTranslation } from 'react-i18next';
 
 interface AddUserDialogProps {
   open: boolean;
@@ -13,6 +14,7 @@ interface AddUserDialogProps {
 }
 
 const AddUserDialog: React.FC<AddUserDialogProps> = ({ open, close }) => {
+  const { t } = useTranslation();
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
   const [name, setName] = useState('');
@@ -29,7 +31,7 @@ const AddUserDialog: React.FC<AddUserDialogProps> = ({ open, close }) => {
     try {
       create({ createUserDto: { login, password, name, enabled, admin: admin && enabled } }).unwrap();
     } catch (e) {
-      const text = e instanceof Error ? e.message : `got unknown error while creating user`;
+      const text = e instanceof Error ? e.message : t(`got unknown error while creating user`);
       dispatch(addSnackbar({ severity: 'error', text }));
     }
   };
@@ -46,10 +48,10 @@ const AddUserDialog: React.FC<AddUserDialogProps> = ({ open, close }) => {
   return (
     <CustomDialog
       open={!!open}
-      title={`Add user`}
+      title={t(`Add user`)}
       close={handleClose}
       onConfirm={handleCreate}
-      confirmButtonText='Create'
+      confirmButtonText={t('Create')}
       confirmButtonProps={{ disabled: !valid }}
       content={
         <LoadingWrapper loading={isLoading} error={isError}>
@@ -57,18 +59,18 @@ const AddUserDialog: React.FC<AddUserDialogProps> = ({ open, close }) => {
             sx={{ mt: 1 }}
             fullWidth
             required
-            label='Login'
+            label={t('Login')}
             value={login}
             onChange={({ target: { value } }) => setLogin(value)}
             onKeyDown={e => e.stopPropagation()}
             error={!login || duplicateLogin}
-            helperText={duplicateLogin ? 'Duplicate login' : undefined}
+            helperText={duplicateLogin ? t('Duplicate login') : undefined}
           />
           <CustomPassword
             sx={{ mt: 1 }}
             fullWidth
             required
-            label='Password'
+            label={t('Password')}
             value={password}
             onChange={setPassword}
             onKeyDown={e => e.stopPropagation()}
@@ -77,19 +79,19 @@ const AddUserDialog: React.FC<AddUserDialogProps> = ({ open, close }) => {
           <TextField
             sx={{ mt: 1 }}
             fullWidth
-            label='Name'
+            label={t('Name')}
             value={name}
             onChange={({ target: { value } }) => setName(value)}
             onKeyDown={e => e.stopPropagation()}
           />
           <FormControlLabel
             control={<Switch checked={enabled} onChange={(_, checked) => setEnabled(checked)} />}
-            label='Enabled'
+            label={t('Enabled')}
           />
           <FormControlLabel
             disabled={!enabled}
             control={<Switch checked={enabled && admin} onChange={(_, checked) => setAdmin(checked)} />}
-            label='Admin'
+            label={t('Admin')}
           />
         </LoadingWrapper>
       }
