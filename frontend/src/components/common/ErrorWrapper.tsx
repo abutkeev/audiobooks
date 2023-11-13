c6b95c337514188af14277db2836584dcd272992
Alexey Butkeev <abutkeev@netup.ru>
2023-11-13 15:39:50 +0300

    (front) error wraper localization

--- frontend/src/components/common/ErrorWrapper.tsx
+++ frontend/src/components/common/ErrorWrapper.tsx
@@ -1,10 +1,13 @@
 import { Alert, Button } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
 export interface ErrorWrapperProps {
   error?: boolean;
 }
 
 const ErrorWrapper: React.FC<React.PropsWithChildren<ErrorWrapperProps>> = ({ children, error }) => {
+  const { t } = useTranslation();
+
   if (!error) return children;
 
   return (
@@ -13,11 +16,11 @@ const ErrorWrapper: React.FC<React.PropsWithChildren<ErrorWrapperProps>> = ({ ch
       variant='outlined'
       action={
         <Button variant='contained' color='primary' onClick={() => document.location.reload()}>
-          Reload page
+          {t('Reload page')}
         </Button>
       }
     >
-      An error occurred
+      {t('An error occurred')}
     </Alert>
   );
 };
