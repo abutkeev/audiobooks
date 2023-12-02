2078a94e243bd21115fe8cd6442a4c836ff2fe15
Alexey Butkeev <abutkeev@netup.ru>
2023-12-02 21:10:33 +0300

    :pencil: (front) set series page title

--- frontend/src/pages/series/index.tsx
+++ frontend/src/pages/series/index.tsx
@@ -4,10 +4,12 @@ import LoadingWrapper from '@/components/common/LoadingWrapper';
 import { FC } from 'react';
 import { useTranslation } from 'react-i18next';
 import SeriesItem from './SeriesItem';
+import useTitle from '@/hooks/useTitle';
 
 const Series: FC = () => {
   const { data: series = [], isLoading, isError } = useSeriesGetQuery();
   const { t } = useTranslation();
+  useTitle(t('Series'));
 
   return (
     <LoadingWrapper loading={isLoading} error={isError}>
