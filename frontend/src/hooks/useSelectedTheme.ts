347a695babb1d837a64005f325c1bfe4e79f9e9c
Alexey Butkeev <abutkeev@netup.ru>
2023-12-02 19:47:29 +0300

    :ambulance: (front) fix auth check in useSelectedTheme hook

--- frontend/src/hooks/useSelectedTheme.ts
+++ frontend/src/hooks/useSelectedTheme.ts
@@ -1,5 +1,5 @@
 import { useLazyProfileGetSettingsQuery, useProfileSetSettingsMutation } from '@/api/api';
-import auth from '@/store/features/auth';
+import useAuthData from './useAuthData';
 import { TextFieldProps } from '@mui/material';
 import { useEffect, useState } from 'react';
 
@@ -7,6 +7,7 @@ const useSelectedTheme = () => {
   const [getSettings, { status }] = useLazyProfileGetSettingsQuery();
   const [setSettings] = useProfileSetSettingsMutation();
   const [theme, setTheme] = useState(localStorage.getItem('theme') || 'auto');
+  const auth = useAuthData();
 
   useEffect(() => {
     if (!auth || !['uninitialized', 'fulfilled'].includes(status)) return;
