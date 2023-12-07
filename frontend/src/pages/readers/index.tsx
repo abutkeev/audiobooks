060d65100f866afd52f78f00c3bdacecc4acb438
Alexey Butkeev <abutkeev@netup.ru>
2023-12-07 10:54:26 +0300

    (front) add search to readers

--- frontend/src/pages/readers/index.tsx
+++ frontend/src/pages/readers/index.tsx
@@ -1,19 +1,27 @@
 import { useReadersGetQuery } from '@/api/api';
 import EmptyListWrapper from '@/components/common/EmptyListWrapper';
 import LoadingWrapper from '@/components/common/LoadingWrapper';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import ReaderItem from './ReaderItem';
 import useTitle from '@/hooks/useTitle';
+import useSearchMatcher from '@/hooks/useSearchMatcher';
 
 const Readers: FC = () => {
-  const { data: readers = [], isLoading, isError } = useReadersGetQuery();
+  const { data = [], isLoading, isError } = useReadersGetQuery();
   const { t } = useTranslation();
   useTitle(t('Readers'));
+  const searchMatcher = useSearchMatcher();
+
+  const readers = useMemo(() => {
+    if (!searchMatcher) return data;
+
+    return data.filter(({ id, name }) => searchMatcher(id, { equels: true }) || searchMatcher(name));
+  }, [data, searchMatcher]);
 
   return (
     <LoadingWrapper loading={isLoading} error={isError}>
-      <EmptyListWrapper wrap={readers.length === 0} message={t('No readers')}>
+      <EmptyListWrapper wrap={readers.length === 0} message={searchMatcher ? t('No readers found') : t('No readers')}>
         {readers.map(item => (
           <ReaderItem key={item.id} item={item} />
         ))}
