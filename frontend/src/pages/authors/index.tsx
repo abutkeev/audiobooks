3191d234ebd37b92a3e49a1dc3f8f80c4b3e6904
Alexey Butkeev <abutkeev@netup.ru>
2023-12-07 10:49:19 +0300

    (front) add search to authors

--- frontend/src/pages/authors/index.tsx
+++ frontend/src/pages/authors/index.tsx
@@ -1,19 +1,27 @@
 import { useAuthorsGetQuery } from '@/api/api';
 import EmptyListWrapper from '@/components/common/EmptyListWrapper';
 import LoadingWrapper from '@/components/common/LoadingWrapper';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import AuthorItem from './AuthorItem';
 import useTitle from '@/hooks/useTitle';
+import useSearchMatcher from '@/hooks/useSearchMatcher';
 
 const Authors: FC = () => {
-  const { data: authors = [], isLoading, isError } = useAuthorsGetQuery();
+  const { data = [], isLoading, isError } = useAuthorsGetQuery();
   const { t } = useTranslation();
   useTitle(t('Authors'));
+  const searchMatcher = useSearchMatcher();
+
+  const authors = useMemo(() => {
+    if (!searchMatcher) return data;
+
+    return data.filter(({ id, name }) => searchMatcher(id, { equels: true }) || searchMatcher(name));
+  }, [data, searchMatcher]);
 
   return (
     <LoadingWrapper loading={isLoading} error={isError}>
-      <EmptyListWrapper wrap={authors.length === 0} message={t('No authors')}>
+      <EmptyListWrapper wrap={authors.length === 0} message={searchMatcher ? t('No authors found') : t('No authors')}>
         {authors.map(item => (
           <AuthorItem key={item.id} item={item} />
         ))}
