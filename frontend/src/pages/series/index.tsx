e4caab385c8e17eac5b21064b9633b8ff91c395d
Alexey Butkeev <abutkeev@netup.ru>
2023-12-07 11:05:06 +0300

    (front) add search to series

--- frontend/src/pages/series/index.tsx
+++ frontend/src/pages/series/index.tsx
@@ -1,19 +1,27 @@
 import { useSeriesGetQuery } from '@/api/api';
 import EmptyListWrapper from '@/components/common/EmptyListWrapper';
 import LoadingWrapper from '@/components/common/LoadingWrapper';
-import { FC } from 'react';
+import { FC, useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import SeriesItem from './SeriesItem';
 import useTitle from '@/hooks/useTitle';
+import useSearchMatcher from '@/hooks/useSearchMatcher';
 
 const Series: FC = () => {
-  const { data: series = [], isLoading, isError } = useSeriesGetQuery();
+  const { data = [], isLoading, isError } = useSeriesGetQuery();
   const { t } = useTranslation();
   useTitle(t('Series'));
+  const searchMatcher = useSearchMatcher();
+
+  const series = useMemo(() => {
+    if (!searchMatcher) return data;
+
+    return data.filter(({ id, name }) => searchMatcher(id, { equels: true }) || searchMatcher(name));
+  }, [data, searchMatcher]);
 
   return (
     <LoadingWrapper loading={isLoading} error={isError}>
-      <EmptyListWrapper wrap={series.length === 0} message={t('No series')}>
+      <EmptyListWrapper wrap={series.length === 0} message={searchMatcher ? t('No series found') : t('No series')}>
         {series.map(item => (
           <SeriesItem key={item.id} item={item} />
         ))}
