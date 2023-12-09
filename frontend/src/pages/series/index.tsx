493dacc38b442f999abbea3ebd0dbedfdaf3d8ba
Alexey Butkeev <abutkeev@netup.ru>
2023-12-09 04:50:53 +0300

    (front) add series search by author

--- frontend/src/pages/series/index.tsx
+++ frontend/src/pages/series/index.tsx
@@ -6,21 +6,31 @@ import { useTranslation } from 'react-i18next';
 import SeriesItem from './SeriesItem';
 import useTitle from '@/hooks/useTitle';
 import useSearchMatcher from '@/hooks/useSearchMatcher';
+import useAuthors from '@/hooks/useAuthors';
 
 const Series: FC = () => {
   const { data = [], isLoading, isError } = useSeriesGetQuery();
+  const { authors, authorsLoading, authorsError } = useAuthors();
   const { t } = useTranslation();
   useTitle(t('Series'));
   const searchMatcher = useSearchMatcher();
 
   const series = useMemo(() => {
     if (!searchMatcher) return data;
+    const filtredAuhorsIds = Object.entries(authors)
+      .filter(([, name]) => searchMatcher(name))
+      .map(([id]) => id);
 
-    return data.filter(({ id, name }) => searchMatcher(id, { equels: true }) || searchMatcher(name));
-  }, [data, searchMatcher]);
+    return data.filter(
+      ({ id, name, authors }) =>
+        searchMatcher(id, { equels: true }) ||
+        searchMatcher(name) ||
+        authors.some(author_id => filtredAuhorsIds.includes(author_id))
+    );
+  }, [data, authors, searchMatcher]);
 
   return (
-    <LoadingWrapper loading={isLoading} error={isError}>
+    <LoadingWrapper loading={isLoading || authorsLoading} error={isError || authorsError}>
       <EmptyListWrapper wrap={series.length === 0} message={searchMatcher ? t('No series found') : t('No series')}>
         {series.map(item => (
           <SeriesItem key={item.id} item={item} />
